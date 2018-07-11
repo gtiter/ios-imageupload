b833a9c1d54e372bc6fd46d7827e7378913705b2
黄超 <huangchao@macbook.local>
2018-07-11 23:07:55 +0900

    Initial Commit

---
+++ singleViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  singleViewTest
+//
+//  Created by 黄超 on 2018/07/11.
+//  Copyright © 2018年 黄超. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
