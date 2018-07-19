bf8af257719558289fc2de169be73c22afa2ab79
黄超 <huangchao@MacBook.local>
2018-07-20 00:12:49 +0900

    first commit

--- singleViewTest/ViewController.swift
+++ singleViewTest/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+class ViewController: UIViewController ,UIWebViewDelegate{
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBOutlet weak var lbltxt: UILabel!
 
-
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.webView.delegate = self
+        //let url = URL(string: "https://www.google.com")
+        if let url = Bundle.main.url(forResource: "index", withExtension: "html") {
+            webView.loadRequest(URLRequest(url: url))
+        }
+        //webView.loadRequest(URLRequest(url: url!))
+    }
+    
+    @IBAction func press(_ sender: UIButton) {
+        lbltxt.text = "button clicked"
+        
+    }
 }
 
